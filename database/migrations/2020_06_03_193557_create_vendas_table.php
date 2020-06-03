aed25fa35f66d2134bec690bfc52515fbccc25d7
Anderson Lairton <lairton.anderson@gmail.com>
2020-06-03 19:11:04 -0300

    criando models,requests,controlers e migrations

---
+++ database/migrations/2020_06_03_193557_create_vendas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_cliente')->unsigned();
+            $table->foreign('id_cliente')
+                ->references('id')
+                ->on('clientes')
+                ->onDelete('cascade');
+            $table->float('valor',5,2);
+            $table->date('data_venda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+}
---
+++ database/migrations/2020_06_03_200253_create_itens_vendas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItensVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itens_vendas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantidade_vendida');
+            $table->bigInteger('id_produto')->unsigned();
+            $table->foreign('id_produto')
+                ->references('id')
+                ->on('produtos')
+                ->onDelete('cascade');
+            $table->bigInteger('id_venda')->unsigned();
+            $table->foreign('id_venda')
+                ->references('id')
+                ->on('vendas')
+                ->onDelete('cascade');
+            $table->date('data_venda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_vendas');
+    }
+}
