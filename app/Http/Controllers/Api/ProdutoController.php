e9c8c360da9c4d90a30bc2e96b46716e1cc2855d
Anderson Lairton <lairton.anderson@gmail.com>
2020-06-03 22:02:57 -0300

    alterandoa nome dos controllers

--- app/Http/Controllers/Api/ProdutoController.php
+++ app/Http/Controllers/Api/ProdutoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ClienteControler extends Controller
+class ProdutoController extends Controller
 {
     //
 }
