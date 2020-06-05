45a4426b5a34a7c0d88d8afb2f00e5f163d8ea61
Anderson Lairton <lairton.anderson@gmail.com>
2020-06-05 18:46:05 -0300

    definindo fillable do model venda

--- app/Model/Venda.php
+++ app/Model/Venda.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venda extends Model
 {
+    protected $table = 'vendas';
+    public $timestamps = true;
+    protected $fillable = ['id_cliente', 'valor'];
 
     public function clientes(){
         return $this->belongsTo(Cliente::class,'id');
