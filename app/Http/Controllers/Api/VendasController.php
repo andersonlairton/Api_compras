758f400ede0a5be9453c7a229e044d47fe6c3cc3
Anderson Lairton <lairton.anderson@gmail.com>
2020-06-06 16:44:57 -0300

    implementando o metodo de alteração de cliente da venda

--- app/Http/Controllers/Api/VendasController.php
+++ app/Http/Controllers/Api/VendasController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\VendaRequest;
+use App\Model\Cliente;
+use App\Model\Venda;
+use Exception;
 use Illuminate\Http\Request;
 
 class VendasController extends Controller
@@ -14,7 +18,9 @@ class VendasController extends Controller
      */
     public function index()
     {
-        //
+        $vendas = Venda::all();
+
+        return $vendas;
     }
 
     /**
@@ -22,9 +28,17 @@ class VendasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(VendaRequest $dados)
     {
-        //
+        $cliente = Cliente::find($dados->id_cliente);
+        if (empty($cliente)) {
+            return ['erro' => 'cliente invalido'];
+        }
+        $v =  Venda::create($dados->all());
+        return [
+            'status' => 'suesso',
+            'nova venda gravada' => $v->id
+        ];
     }
 
     /**
@@ -46,7 +60,12 @@ class VendasController extends Controller
      */
     public function show($id)
     {
-        //
+        $venda = Venda::find($id);
+        if (empty($venda)) {
+            return ['erro' => 'id invalido'];
+        }
+        return $venda;
+        // return ['status'=>'metodo show vendas com o id '.$id];
     }
 
     /**
@@ -69,7 +88,36 @@ class VendasController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        return ['status' => 'metodo update'];
+    }
+    public function alterarCliente(Request $dados, $id)
+    {
+        $venda = $this->show($id);
+        
+        if (empty($venda->data_venda)&&!isset($venda['erro'])) {
+            $cliente = Cliente::find($dados->id_cliente);
+            if (!empty($cliente)) {
+                try {
+                    $venda->update($dados->all());
+                    return ['status'=>'cliente alterado com sucesso'];
+                } catch (Exception $erro) {
+                    return $erro;
+                }
+            } else {
+                return ['erro' => 'cliente invalido'];
+            }
+        }else if(isset($venda['erro']))
+        {
+            return ['erro'=>'venda nao existe no banco de dados'];
+        } 
+        else {
+            return ['status' => 'venda finaliza,cliente nao pode ser alterado'];
+        }
+    }
+
+    public function finalizarvenda($id)
+    {
+        return ['status' => 'metodo finalizar venda'];
     }
 
     /**
