5d11fe3b81ca69864dee5255fe022e8582b4cb34
Anderson Lairton <lairton.anderson@gmail.com>
2020-06-07 14:05:38 -0300

    correção no nome da tabela

--- app/Model/ItensVenda.php
+++ app/Model/ItensVenda.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItensVenda extends Model
 {
-    protected $table = 'itens_venda';
+    protected $table = 'itens_vendas';
     public $timestamps = true;
     protected $fillable = ['quantidade_vendida','id_produto','id_venda','data_venda'];
 }
